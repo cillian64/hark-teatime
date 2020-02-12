32c41866cb1a302991032db662fab8aa70b6b6cc
HarkonenBade <github@harkonen.net>
2020-02-12 22:06:59 +0000

    More tweaks and v1 version of board

--- circle.py
+++ circle.py
@@ -12,27 +12,16 @@ def main():
         print(m.GetReference())
         if m.GetReference()[0] == "D":
             _id = int(m.GetReference()[1:])
-            (x, y, rot) = id_to_pos(_id, 45.0)
+            theta = (360.0/32.0)*(_id - 1);
+            thetar = math.radians(theta)
+            x = xoff - 45.0 * math.sin(thetar)
+            y = yoff - 45.0 * math.cos(thetar)
             m.SetPosition(pcbnew.wxPointMM(x, y))
-            m.SetOrientation(-(rot-90.0)*10.0)
-        if m.GetReference()[0] == "R":
-            _id = int(m.GetReference()[1:])
-            if( _id < 9):
-                m.SetOrientationDegrees(90)
-            if( _id >= 9 and _id < 17):
-                m.SetOrientationDegrees(0)
-            if( _id >= 17 and _id < 25):
-                m.SetOrientationDegrees(-90)
-            if( _id >= 25):
-                m.SetOrientationDegrees(-180)
-
+            m.SetOrientation((theta+90.0)*10.0)
         m = m.Next()
 
     pcbnew.Refresh()
 
 def id_to_pos(_id, r):
-    theta = (360.0/32.0)*(_id - 1);
-    thetar = math.radians(theta)
-    x = r * math.sin(thetar)
-    y = r * math.cos(thetar)
+    
     return (x+xoff, yoff-y, theta);
