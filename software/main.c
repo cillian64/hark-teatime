c2c30c198508fb64858112b1806a713786fa3e51
Hark <github@harkonen.net>
2020-09-06 14:02:01 +0100

    Initial version of firmware

--- software/main.c
+++ software/main.c
@@ -1,61 +1,112 @@
 #include <avr/io.h>
 #define F_CPU 1E6 				// 1MHz
 #include <util/delay.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
 
-#define LED_RED PB0
-#define LED_YEL PB1
-#define LED_GRE PB2
-#define TRIGGER PB3
+#define RCLK PB0
+#define SCLK PB1
+#define SER PB2
 #define BUZZER  PB4
 
-#define ON(PIN) PORTB &= ~(1 << PIN)
-#define OFF(PIN) PORTB |= (1 << PIN)
+#define OFF(PIN) PORTB &= ~(1 << PIN)
+#define ON(PIN) PORTB |= (1 << PIN)
 
-inline setup_pins(void) {
-    DDRB |= _BV(LED_RED) | _BV(LED_YEL) | _BV(LED_GRE) | _BV(BUZZER);
-    OFF(LED_RED);
-    OFF(LED_GRE);
-    OFF(LED_YEL);
+inline void setup_pins(void) {
+    DDRB |= _BV(RCLK) | _BV(SCLK) | _BV(SER) | _BV(BUZZER) | _BV(PB3);
+    OFF(RCLK);
+    OFF(SCLK);
+    OFF(SER);
+    OFF(PB3);
     OFF(BUZZER);
 }
 
+inline void display(char sec, char block) {
+    char init_off;
+    char mid_off;
+    char i;
+
+    if(sec < 2) {
+        init_off = 0;
+    } else {
+        init_off = sec - 1;
+    }
+
+    if(sec > 0 && sec <= (32 - block)) {
+        mid_off = 32 - block - init_off - 1;
+    } else {
+        mid_off = 32 - block;
+    }
+
+    OFF(RCLK);
+    if(sec > 0 && sec <= (32 - block)){
+        for(i = 0; i < init_off; i++){
+            OFF(SCLK);
+            ON(SER);
+            ON(SCLK);
+        }
+        OFF(SCLK);
+        OFF(SER);
+        ON(SCLK);
+    }
+    for(i = 0; i < mid_off; i++){
+        OFF(SCLK);
+        ON(SER);
+        ON(SCLK);
+    }
+    for(i = 0; i < block; i++){
+        OFF(SCLK);
+        OFF(SER);
+        ON(SCLK);
+    }
+    ON(RCLK);
+}
+
+inline void sleep_forever() {
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    cli();
+    sleep_enable();
+    sleep_bod_disable();
+    sei();
+    sleep_cpu();
+}
+
+static inline void buzz500ms() {
+    char c1, c2;
+    for(c1 = 0; c1 < 127; c1++) {
+        for(c2 = 0; c2 < 8; c2++) {
+            ON(BUZZER);
+            _delay_us(250);
+            OFF(BUZZER);
+            _delay_us(250);
+        }
+    }
+}
+
 int main(void) {
+    char total;
+    char inner;
     char i;
     
     setup_pins();
 
-    while (1) {
-        if((PINB & _BV(TRIGGER)) == 0) {
-            ON(LED_GRE);
-            for(i=0; i<62; i++){
-                _delay_ms(1000);
-            }
-            OFF(LED_GRE);
-            ON(LED_YEL);
-            for(i=0; i<62; i++){
-                _delay_ms(1000);
-            }
-            OFF(LED_YEL);
-            ON(LED_RED);
-            for(i=0; i<62; i++){
-                _delay_ms(1000);
-            }
-            OFF(LED_RED);
-            ON(BUZZER);
-            for(i=0; i<12; i++){
-                if(i%2 == 0) {
-                    ON(LED_RED);
-                    ON(LED_YEL);
-                    ON(LED_GRE);
-                } else {
-                    OFF(LED_RED);
-                    OFF(LED_YEL);
-                    OFF(LED_GRE);
+    while(1) {
+        for(total = 0; total < 32; total++) {
+            for(inner = 0; inner < 10; inner++) {
+                for(i = 1; i < 33; i++) {
+                    display(i, total);
+                    _delay_ms(31);
                 }
-                _delay_ms(250);
+                _delay_ms(8);   
             }
-            OFF(BUZZER);
         }
+        for(i = 0; i < 5; i++) {
+            display(0,32);
+            buzz500ms();
+            display(0,0);
+            _delay_ms(500);
+        }
+        sleep_forever();
     }
 }		
 
