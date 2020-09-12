d9c5a2d8c6d9517b46c6c194585b05118ee1402c
HarkonenBade <github@harkonen.net>
2020-09-12 14:34:28 +0100

    SMD variant board

--- circle.py
+++ circle.py
@@ -11,13 +11,23 @@ def main():
     while m is not None:
         print(m.GetReference())
         if m.GetReference()[0] == "D":
+            print("D inner")
             _id = int(m.GetReference()[1:])
             theta = (360.0/32.0)*(_id - 1);
             thetar = math.radians(theta)
-            x = xoff - 45.0 * math.sin(thetar)
-            y = yoff - 45.0 * math.cos(thetar)
+            x = xoff - 23.5 * math.sin(thetar)
+            y = yoff - 23.5 * math.cos(thetar)
             m.SetPosition(pcbnew.wxPointMM(x, y))
             m.SetOrientation((theta+90.0)*10.0)
+        if m.GetReference()[0] == "R":
+            print("R inner")
+            _id = int(m.GetReference()[1:])
+            theta = (360.0/32.0)*(_id - 1);
+            thetar = math.radians(theta)
+            x = xoff - 21 * math.sin(thetar)
+            y = yoff - 21 * math.cos(thetar)
+            m.SetPosition(pcbnew.wxPointMM(x, y))
+            m.SetOrientation((theta+180)*10.0)
         m = m.Next()
 
     pcbnew.Refresh()
